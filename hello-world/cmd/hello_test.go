7e5f4017e85c4892fa9629b8c3685ac8cafb2734
kompilator <roman.skrilioff@gmail.com>
2021-09-16 11:32:47 +0300

    init project

---
+++ hello-world/cmd/hello_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want % q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "Spanish")
+		want := "Hola, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is suplied", func(t *testing.T) {
+		got := Hello("", "Spanish")
+		want := "Hola, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("World", "French")
+		want := "Bonjour, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
