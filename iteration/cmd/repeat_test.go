b06e24f17b0f939e0e0ca9916229074baf44de7b
kompilator <roman.skrilioff@gmail.com>
2021-09-17 15:59:59 +0300

    practice exercises

--- iteration/cmd/repeat_test.go
+++ iteration/cmd/repeat_test.go
@@ -1,14 +1,15 @@
 package iteration
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
-	expected := "aaaaa"
+	expected := repeatCount
 
-	if repeated != expected {
+	if len(repeated) != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
@@ -17,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a")
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a")
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
