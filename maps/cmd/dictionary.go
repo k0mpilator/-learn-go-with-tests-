ecb07a28dda82414a81e3e189234e05503d396f0
kompilator <roman.skrilioff@gmail.com>
2021-10-13 13:03:06 +0300

    add yet another error type for when the word does not exist

--- maps/cmd/dictionary.go
+++ maps/cmd/dictionary.go
@@ -1,8 +1,9 @@
 package main
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -35,6 +36,7 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, definition string) error {
 	d[word] = definition
+	return nil
 }
